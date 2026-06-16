2d40c19a84d3d34b17014f060e107b45c11d9699
Tassadit <tassaditaksas03@gmail.com>
2026-06-16 21:00:52 +0200

    client routes

--- server/src/routes/clientRoutes.js
+++ server/src/routes/clientRoutes.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getClients,
-  getClient,
-  createClient,
-  updateClient,
-  deleteClient
-} = require('../controllers/clientController');
+const { getClients, getClient, createClient, updateClient, deleteClient } = require('../controllers/clientController');
 
 router.get('/', getClients);
 router.get('/:id', getClient);
