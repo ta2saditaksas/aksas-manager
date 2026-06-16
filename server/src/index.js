d0c23e4e0e0fa4cc4818156afaacd19db950fac7
Tassadit <tassaditaksas03@gmail.com>
2026-06-16 21:05:00 +0200

    v

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -15,26 +14,24 @@ app.use(express.json());
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
-// Middleware de protection
+// Middleware protection
 const { protect } = require('./middlewares/authMiddleware');
 
 // Routes protégées
 const clientRoutes = require('./routes/clientRoutes');
 const commandeRoutes = require('./routes/commandeRoutes');
-const stockRoutes = require('./routes/stockRoutes');
-const paiementRoutes = require('./routes/paiementRoutes');
+const devisRoutes = require('./routes/devisRoutes');
+const livraisonRoutes = require('./routes/livraisonRoutes');
 
 app.use('/api/clients', protect, clientRoutes);
 app.use('/api/commandes', protect, commandeRoutes);
-app.use('/api/stocks', protect, stockRoutes);
-app.use('/api/paiements', protect, paiementRoutes);
+app.use('/api/devis', protect, devisRoutes);
+app.use('/api/livraisons', protect, livraisonRoutes);
 
-// Route de test
 app.get('/', (req, res) => {
   res.json({ message: 'Aksas Manager API fonctionne ' });
 });
 
-// Démarrer le serveur
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
